ac8352e70c87435c18764660685c230ebc4ccae0
suhyunch <b215220@daum.net>
2017-11-14 01:18:34 +0900

    dfs

--- BOJ/1697/src.cpp
+++ BOJ/1697/src.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-int N, K,t;
+int N, K, t;
 bool visit[100000];
 
-int dfs(int x, int t)
+void bfs(int x)
 {
-    visit[x]=1;
-    if(x==K) return;
-    int mint=0;
-    if(!visit[x-1] && x+1<=100000 && x-1>=100000 && 2*x<=100000)
-        {mint = dfs(x-1,t+1);}
-    if(!visit[x+1] && x+1<=100000 && x-1>=100000 && 2*x<=100000)
-        {dfs(x+1,t+1);}
-    if(!visit[x*2] && x+1<=100000 && x-1>=100000 && 2*x<=100000)
-        {dfs(x*2,t+1);}
+    queue<int> q;
+    q.push(x);
+    while(!q.empty())
+    {
+        x = q.front();
+        visit[x]=1;
+        cout << x << endl;
+        q.pop();
+        t++;
+        if(x==K) break;
+        if(visit[x]) continue;
+        if(x-1>=0 && x+1<=100000 && x*2 <=100000)
+        {
+            if(!visit[x-1]) q.push(x-1);
+            if(!visit[x+1]) q.push(x+1);
+            if(!visit[2*x]) q.push(x*2);
+        }
+        else if(x==0) if(!visit[x+1]) q.push(x+1);
+        else if(x*2>100000 && x+1<=100000)
+        {
+            if(!visit[x-1]) q.push(x-1);
+            if(!visit[x+1]) q.push(x+1);
+        }
+        else if(x==100000) if(!visit[x-1]) q.push(x-1);
+
+    }
+
 }
 int main()
 {
 
     cin >> N >> K;
 
-
-    cout << dfs(N,0);
+    bfs(N);
+    cout << t;
 }
