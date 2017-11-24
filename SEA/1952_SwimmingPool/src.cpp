dea7480eb287e2dcb404ec7d70b4f3798ec42cd2
suhyunch <b215220@daum.net>
2017-11-24 15:41:03 +0900

    1952 수영장 dp tc48/50

--- SEA/1952_SwimmingPool/src.cpp
+++ SEA/1952_SwimmingPool/src.cpp
@@ -28,21 +28,11 @@ int main()
          d[1][1]=0;
          d[1][2]=0;
       }
-
-      for(int i=1; i<=12; i++)
-      {
-        for(int j=0; j<3;j++)
-          cout << d[i][j] << "  ";
-        cout << endl;
-      }
-      cout << endl;
-
     for(int i=2; i<=12; i++)
     {
       bool x;
       if(plan[i]==0) x=0;
       else x=1;
-      cout << "x : "<<x << endl;
       if(i<4)
       {
         d[i][0]=min(d[i-1][0],d[i-1][1])+plan[i]*cost[0];
@@ -53,30 +43,15 @@ int main()
     {
       d[i][0]=min(d[i-1][0],d[i-1][1],d[i-1][2])+plan[i]*cost[0];
       d[i][1]=min(d[i-1][0],d[i-1][1],d[i-1][2])+cost[1]*x;
-        //d[i][2]는 3가지 경우가 각각 2,2,3가지 총 7가지 경우 가능
         int cmp1=min(d[i-3][0]+ cost[2],d[i-3][1]+ cost[2],d[i-3][2]+ cost[2]);
         int cmp2=min(d[i-2][0]+ cost[2],d[i-2][1]+ cost[2],d[i-2][2]+ cost[2]);
-        int cmp3=min(d[i-1][0]+ cost[2]*x,d[i-1][1]+ cost[2]*x,d[i-1][2]+ cost[2]*x);
+        int cmp3=min(d[i-1][0]+ cost[2],d[i-1][1]+ cost[2],d[i-1][2]+ cost[2]);
       d[i][2]=min(cmp1,cmp2,cmp3) ;
     }
-    cout << i << endl;
-    for(int i=1; i<=12; i++)
-    {
-      for(int j=0; j<3;j++)
-        cout << d[i][j] << "  ";
-      cout << endl;
-    }
-    cout << endl;
-  }
-  for(int i=1; i<=12; i++)
-  {
-    for(int j=0; j<3;j++)
-      cout << d[i][j] << "  ";
-    cout << endl;
   }
   int min_cost=cost[3];
   for(int i=0; i<3; i++)
-    if(d[12][i]< min_cost) min_cost=d[12][i];
+    if(d[12][i]<= min_cost) min_cost=d[12][i];
 
   cout << "#" << T << " "<<  min_cost << endl;
  }
