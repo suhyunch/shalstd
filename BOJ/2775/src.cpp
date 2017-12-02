222c36c0a7cc124597103a7f83b7c66d1722d2e0
suhyunch <b215220@daum.net>
2017-12-02 13:00:44 +0900

    2775 부녀회장이 될테야 dp 수정

--- BOJ/2775/src.cpp
+++ BOJ/2775/src.cpp
@@ -9,17 +9,19 @@ int main()
   int T;
   cin >> T;
 
-  for(int i=1; i<15; i++)
-  { apart[0][i]=i;  }
   for(int tc=0; tc<T; tc++)
   {
     int k, n;
     cin >> k >> n;
+
     for(int a=1; a<=k; a++)
     {
-      apart[a][1]=apart[a-1][1];
-      for(int b=2; b<=n; b++)
-        apart[a][b]=apart[a-1][b]+apart[a][b-1];
+      apart[a][1]=1;
+      for(int b=1; b<=n; b++)
+      {
+          if(a==1) apart[a][b]=b+apart[a][b-1];
+          else apart[a][b]=apart[a-1][b]+apart[a][b-1];
+      }
     }
 
     cout << apart[k][n] << endl;
