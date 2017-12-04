160c7c6bf2898a27ad2c3d5cb8457c6ce5faefda
suhyunch <b215220@daum.net>
2017-12-04 22:24:40 +0900

    5622 다이얼 (문자열 처리)

---
+++ BOJ/5622/src.cpp
@@ -0,0 +1,23 @@
+//https://www.acmicpc.net/problem/5622
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string num;
+  cin >> num;
+  int x;
+  int l=num.size();
+  int sum=0;
+  for(int i=0; i<l; i++)
+  {
+    if(num[i]=='1') x=2;
+    else if(num[i]=='Z' ) x= 10;
+    else if((int)num[i]>='S')  x= (num[i]-'A'-1)/3+3;
+    else x= (num[i]-'A')/3+3;
+    cout << num[i] << "  " << x  << endl;
+    sum+=x;
+  }
+  cout << sum;
+}
