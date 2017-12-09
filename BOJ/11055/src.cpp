57f020fdcd3f1f9fbef80d6e754943c4be0ee392
suhyunch <b215220@daum.net>
2017-12-09 12:43:31 +0900

    11055 가장 큰 증가 부분 수열 (dp)

---
+++ BOJ/11055/src.cpp
@@ -0,0 +1,33 @@
+//https://www.acmicpc.net/problem/11055
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     int n;
+     cin >> n;
+
+     int A[1001];
+     int dp[1001];
+     int x,max,index;
+     for(int i=1; i<=n; i++) cin >> A[i];
+     dp[1]=A[1];
+     for(int i=2; i<=n; i++)
+     {
+          max=0;
+          for(int j=i-1; j>0; j--)
+          {
+               x=A[i];
+               if(A[i]>A[j]) x=dp[j]+A[i];
+               if(max<x) { max=x; index=j;}
+          }
+          dp[i]=max;
+
+     }
+
+     max=0;
+     for(int i=1; i<=n; i++)
+          if(max<dp[i]) max=dp[i];
+
+     cout << max;
+}
