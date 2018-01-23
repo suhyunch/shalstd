3a79fe86f5f400757f7c792a05e9b13fe31f9c06
suhyunch <b215220@daum.net>
2018-01-23 15:40:48 +0900

    1937 욕심쟁이 판다 (dp)

--- BOJ/1000~/1937/src.cpp
+++ BOJ/1000~/1937/src.cpp
@@ -1,48 +1,46 @@
 //https://www.acmicpc.net/problem/1937
 #include <iostream>
 #include <stdio.h>
+#include <algorithm>
 using namespace std;
-long map[502][502];
+int map[502][502];
 int dp[502][502];
 int dy[4] = {1,0,-1,0};
 int dx[4] = {0,1,0,-1};
   int n;
-int max1;
 int mov(int nowy, int nowx)
 {
-     int nexty, nextx;
-     for(int i=0; i<4; i++)
+     if(dp[nowy][nowx]==0)
      {
-          nexty=nowy+dy[i];
-          nextx=nowx+dx[i];
-
-          if((map[nexty][nextx]>map[nowy][nowx]) && (dp[nexty][nextx]<dp[nowy][nowx]+1))
+          dp[nowy][nowx]=1;
+          int nexty, nextx;
+          for(int i=0; i<4; i++)
           {
-                    dp[nexty][nextx]=dp[nowy][nowx]+1;
-                    if(dp[nexty][nextx]>max1) max1=dp[nexty][nextx];
-                    mov(nexty, nextx);
+               nexty=nowy+dy[i];
+               nextx=nowx+dx[i];
+
+               if(map[nexty][nextx]>map[nowy][nowx])
+               {
+                         dp[nowy][nowx]=max(dp[nowy][nowx],mov(nexty,nextx)+1);
+               }
           }
      }
+     return dp[nowy][nowx];
 }
 
 int main()
 {
   scanf("%d", &n);
 
- for(int i=1; i<=n; i++)
+  for(int i=1; i<=n; i++)
     for(int j=1; j<=n; j++)
-     scanf("%d", &map[i][j]);
+     scanf("%d",&map[i][j]);
 
+     int ans=0;
 
      for(int i=1; i<=n; i++)
        for(int j=1; j<=n; j++)
-       {
-            if(dp[i][j]==0) // 탐색한 적이 없으면
-            {
-                 dp[i][j]=1;
-                 if(max1 !=n*n) mov(i,j);
-            }
-       }
+                ans=max(ans,mov(i,j));
 
-       printf("%d",max1);
+       printf("%d",ans);
 }
