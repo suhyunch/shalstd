788a755d1a9bfdf3f545f0d893adc00e602e5f80
suhyunch <b215220@daum.net>
2018-02-27 03:18:44 +0900

    1008 A/B 사칙연산

---
+++ BOJ/1000~/1008/src.cpp
@@ -0,0 +1,11 @@
+//https://www.acmicpc.net/problem/1008
+#include <iostream>
+using namespace std;
+
+int main(){
+    double A, B;
+
+    cin >> A >> B;
+    cout.precision(10);
+    cout << A/B<<endl;
+}
