a249aaf9c8fad1978d229b2c5a4c4061e6e05411
suhyunch <b215220@daum.net>
2018-03-02 04:50:41 +0900

    9506 약수들의 합

---
+++ BOJ/9000~/9506/src.cpp
@@ -0,0 +1,39 @@
+//https://www.acmicpc.net/problem/9506
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void process(int n){
+     int ans=1;
+     int tmp[5000];
+     memset(tmp, 0, sizeof(tmp));
+     tmp[0]=1;
+     int index=1;
+     for(int i=2; i<=n/2; i++){
+          if(n%i==0) {
+               ans+=i;
+               tmp[index++]=i;
+          }
+     }
+
+     if(n==ans){
+          cout << n << " = ";
+          for(int i=0; i<index; i++){
+               cout << tmp[i]<<" ";
+               if(i!=index-1) cout <<"+ ";
+          }
+          cout << endl;
+     }
+     else cout <<n <<" is NOT perfect." <<endl;
+}
+
+int main(){
+      while(1){
+           int n;
+           cin >> n;
+
+           if(n==-1) break;
+
+           process(n);
+      }
+}
