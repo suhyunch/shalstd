44cc837a62c6198780ec39a393dc5cac69f77a73
suhyunch <b215220@daum.net>
2018-03-04 03:40:24 +0900

    2622 삼각형의 길이

---
+++ BOJ/2000~/2622/src.cpp
@@ -0,0 +1,22 @@
+//https://www.acmicpc.net/problem/2622
+#include <iostream>
+using namespace std;
+
+int main(){
+     int n;
+     int cnt=0;
+
+     cin >> n;
+
+     for(int a=1; a<=n; a++){
+          for(int b=a; b<=n; b++){
+               int c=n-(a+b);
+               if(c<b) break;
+               if(c<a+b) cnt++;
+               }
+          }
+     }
+     cout << cnt;
+
+     return 0;
+}
