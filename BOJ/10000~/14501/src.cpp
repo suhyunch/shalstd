96618e73d72ee4fd229f47b9f6c378b2462cee4d
suhyunch <b215220@daum.net>
2018-04-05 01:22:14 +0900

    14501 퇴사(dp)

---
+++ BOJ/10000~/14501/src.cpp
@@ -0,0 +1,45 @@
+//https://www.acmicpc.net/problem/14501
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int mx;
+
+int counsel[16][2];
+int dp[16];
+void input();
+void solve(int today, int earn);
+int main(){
+
+     cin >> N;
+
+     input();
+
+     solve(counsel[1][0], counsel[1][1]);
+     solve(1, 0);
+     for(int i=1; i<=N; i++){
+          if(dp[i]>mx) mx=dp[i];
+     }
+     cout << mx;
+     return 0;
+}
+
+void input(){
+     for(int i=1; i<=N; i++)
+          cin >> counsel[i][0] >> counsel[i][1];
+}
+
+void solve(int today, int earn){
+     if(today<=N){
+          dp[today]=max(dp[today],earn);
+     }
+     if(today>=N){
+          return;
+     }
+
+     if(today+counsel[today+1][0]<=N){
+          solve(today+counsel[today+1][0], earn+counsel[today+1][1]);
+     }
+     solve(today+1, earn);
+}
