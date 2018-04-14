7df8a8e746a72dea99b6ae57818cad6b2be29484
suhyunch <b215220@daum.net>
2018-04-14 15:35:53 +0900

    점심식사 시간

--- samsungsw/2383_lunch/src.cpp
+++ samsungsw/2383_lunch/src.cpp
@@ -1,94 +1,100 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <utility>
+#include <math.h>
 using namespace std;
-int T, N;
-int map[10][10];
-class step{
-public:
-     int y;
-     int x;
-     int l;
-};
-vector<pair<int, int> > k0;
-vector<pair<int, int> > k1;
-step step0;
-step step1;
-int order[10];
 
-int abs(int a, int b){
-     return a>b ? a-b : b-a;
+const int MAXN=11;
+const int MAXT=MAXN*2+MAXN*MAXN;
+//이동시간 2N+10명*최장계단길이10 여유+1?
+struct PT{
+     int r,c;
+     PT() {}
+     PT(int r, int c) : r(r), c(c) {}
+} man[MAXN], stair[2];
+
+int N, M, S;
+//N 방의 한변의 길이
+//M 사람의 수
+//S 계단의 수 , =2
+int map[MAXN][MAXN];
+int match[MAXN];
+int ans;
+
+int dist(int man_idx, int stair_idx){
+     int dx=abs(man[man_idx].r-stair[stair_idx].r);
+     int dy=abs(man[man_idx].c-stair[stair_idx].c);
+     return dx + dy;
 }
 
 void update(){
-     //시간 계산하기
-     for(int t=0; t<)
-}
-void dfs(int M, int pidx){
-     if(pidx==M){
-          update();
-          return;
-     }
-     for(int stair_index=0; stair_index<2; stair_index++){
-          order[pidx]=stair_index;
-          dfs(M, pidx+1);
-     }
-}
+     //각 사람이 어느 계단을 이용할 지 모두 정해졌을 때에 필요한 시간을 계산하는 함수
 
-int main(){
-     cin >> T;
-     for(int TC=1; TC<=T; TC++){
-          cin >> N;
+     int total_min_time=0;
 
+     for(int stair_idx=0; stair_idx<2; stair_idx++){
+          PT &now_stair=stair[stair_idx];
+          int arrival_time[MAXN*2]={0,};
+          //arrival_time[t] 시간 t일 때 계단에 도착하는 사람의 수
+          //최대 도착 시간 :: (0,0) ~ (N-1,N-1) 시간 :: N+N=2N시간
 
-          int pp[10][2];//person position
-          int pc=0;//person count
-          bool sc=0;//step count
-          for(int i=0; i<N; i++){
-               for(int j=0; j<N; j++){
-                    cin >> map[i][j];
-                    if(map[i][j]==1){
-                         pp[pc][0]=i;
-                         pp[pc++][1]=j;
-                    }
-                    if(map[i][j]>=2){
-                         if(sc==0){
-                              step0.y=i;
-                              step0.x=j;
-                              step0.l=map[i][j];
-                              sc=true;
-                         }
-                         else{
-                              step1.y=i;
-                              step1.x=j;
-                              step1.l=map[i][j];
+          int current_stair[MAXT]={0,};
+          //current_stair[t] : 시간 t일 때 계단을 내려가고 있는 사람의 수
+
+          for(int man_idx = 0; man_idx < M; man_idx++){
+               if(match[man_idx] == stair_idx){
+                    arrival_time[dist(man_idx, stair_idx)+1]++;
+               }
+          }
+          int now_min_time=0;
+          //계단에 도착하는 시간 오른차순으로 각 사람이 계단을 내려가도록 시뮬레이션
+          for(int t=1; t<=20; t++){
+               while(arrival_time[t]-->0){
+                    int remain_time=map[now_stair.r][now_stair.c];
+                    for(int walk_time=t; walk_time<MAXT; walk_time++){
+                         if(current_stair[walk_time] < 3){
+                              current_stair[walk_time]++;
+                              remain_time--;
+
+                              if(remain_time==0){
+                                   now_min_time=max(now_min_time, walk_time+1);
+                                   break;
+                              }
                          }
                     }
                }
           }
+          total_min_time=max(total_min_time, now_min_time);
+     }
+     ans=min(ans, total_min_time);
+}
 
-          for(int i=0; i<pc; i++){
-               k0.push_back(make_pair(abs(pp[i][0],step0.y)+abs(pp[i][1],step0.x),i));
-               k1.push_back(make_pair(abs(pp[i][0],step1.y)+abs(pp[i][1],step1.x),i));
-          }
-          sort(k0.begin(), k0.end());
-          sort(k1.begin(), k1.end());
+void dfs(int idx){
+     if(idx==M){
+          update();
+          return;
+     }
+     for(int stair_idx=0; stair_idx<2; stair_idx++){
+          match[idx]=stair_idx;
+          dfs(idx+1);
+     }
+}
 
-          for(int i=0; i<pc; i++){
-               cout << i << " : " << pp[i][0] << " " << pp[i][1] << endl;
-          }
-          for(int i=0; i<pc; i++){
-               cout << k0[i].first << " " << k0[i].second << endl;
-          }
-          cout << endl;
-          for(int i=0; i<pc; i++){
-               cout << k1[i].first << " " << k1[i].second << endl;
-          }
+int main(){
+     int T;
+     cin >> T;
 
-cout <<" step0 - y : " << step0.y << " , x : " << step0.x << " , l : " << step0.l;
-          cout <<" step1 - y : " << step1.y << " , x : " << step1.x << " , l : " << step1.l;
+     for(int tc=1; tc<=T; tc++){
+          cin >>N;
+          M=S=0;
+          for(int i=1; i<=N; i++){
+               for(int j=1; j<=N; j++){
+                    cin >> map[i][j];
+                    if(map[i][j]==1) man[M++]=PT(i, j);
+                    if(map[i][j]>=2) stair[S++]=PT(i, j);
+               }
+          }
+          ans=1e9;
+          dfs(0);
 
-          dfs(pc, 0);
+          cout << "#"<<tc <<" " <<ans<<endl;
      }
 }
